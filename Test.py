4258ac72823bdc335f4f3d01fd03c8dc495e14ec
Ethan <ethan_vn@outlook.com>
2021-05-05 01:39:51 +0800

    初始化仓库

---
+++ Test.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="motifx",
+    version="1.0.0",
+    author="Ethan",
+    author_email="ethan_vn@outlook.com",
+    description="一个简单的四则运算 PyPI 上传例子",
+    long_description="一个简单的 PyPI 上传测试",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JustDoPython/justdopython.github.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
